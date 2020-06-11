8f5406edee051aff5ddab17ebcd689c2d1b47fcb
Kamila Freitas <kamila.silva15@fatec.sp.gov.br>
2020-06-11 02:23:27 -0300

    Emboinha se movendo e atirando.

---
+++ Assets/Script/Bottle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bottle : MonoBehaviour
+{
+   public int speed =6;
+
+    void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+        rb.velocity = new Vector2(0,speed);
+    }
+
+     void OnBecameInvisible ()
+    {
+        Destroy (gameObject);
+    }
+}
---
+++ Assets/Script/Moves.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moves : MonoBehaviour
+{
+
+   public GameObject garrafa;
+   public int speed = 10;
+   void Update() 
+   {
+       float horizontal = Input.GetAxis("Horizontal") * speed *Time.deltaTime;
+       transform.Translate(horizontal, 0, 0); 
+     
+
+       //disparar garrafa
+       if (Input.GetKeyDown(KeyCode.Space))
+       {
+
+        //spawnar bala
+        Instantiate(garrafa,transform.position,Quaternion.identity);
+       }
+   }
+}
