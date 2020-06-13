2f611300ac4ed05084ddb66758e57db449a063e1
Kamila Freitas <kamila.silva15@fatec.sp.gov.br>
2020-06-12 22:00:51 -0300

    emboinha topster

--- Assets/Script/Moves.cs
+++ Assets/Script/Moves.cs
@@ -7,18 +7,47 @@ public class Moves : MonoBehaviour
 
    public GameObject garrafa;
    public int speed = 10;
+   
+   
    void Update() 
    {
-       float horizontal = Input.GetAxis("Horizontal") * speed *Time.deltaTime;
-       transform.Translate(horizontal, 0, 0); 
+       SpawnGarrafa();
+       Move();
+       PreventLeavingScreen();
+       
+       
+    }
+  
+    void SpawnGarrafa()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Instantiate(garrafa,new Vector2(transform.position.x -0.2f,transform.position.y +0.7f),Quaternion.identity);
+            
+        }
+
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed *Time.deltaTime;
+        transform.Translate(horizontal, 0, 0);
+    }
+
+    void PreventLeavingScreen()
+    {
      
+       if(transform.position.x <= -6.9f || transform.position.x >= 6.9f)
 
-       //disparar garrafa
-       if (Input.GetKeyDown(KeyCode.Space))
        {
-
-        //spawnar bala
-        Instantiate(garrafa,transform.position,Quaternion.identity);
+           float PosX = Mathf.Clamp (transform.position.x,-6.9f,6.9f);
+           transform.position = new Vector2(PosX,transform.position.y);
        }
-   }
+    }
+
 }
+
+
+
